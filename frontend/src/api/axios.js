f664ace89d031fbc5a492db247bfdc8abcf7b8aa
DIYA <diya94816@gmail.com>
2026-07-26 20:03:20 +0530

    feat: configure frontend API for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return req;
-});
+);
 
 export default API;
